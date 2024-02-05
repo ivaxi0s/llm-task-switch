66b675fb4652e193bc29940acb0f657921973133
Akash Gupta <ag2118@cam.ac.uk>
2024-02-05 11:03:13 +0000

    experiments: mmlu aa, gsm8k mistral llama

---
+++ scripts/eval_aa/llama.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# This script runs the experiment
+# test set: mmluaa
+# incontext dataset:
+# model: llama-7b
+# NOTE: The test size is *not* limited
+
+conda activate inctxt
+
+MODEL_NAME="llama-7b"
+
+# do not include dailmail, as we have to limit eval_Size
+INCONTEXT_SETS=("mmluaa" "tweetqa" "gigaword")
+
+# For each incontext dataset
+for INCONTEXT_SET in "${INCONTEXT_SETS[@]}"; do
+  echo "Incontext: $INCONTEXT_SET"
+  for i in {0..5..1}; do
+    echo "---Running with $i examples---"
+    python main.py \
+      --eval_data_name mmluaa \
+      --incontext_data_name $INCONTEXT_SET \
+      --num_examples $i \
+      --model_name $MODEL_NAME \
+      --batchsize 1 \
+      --iterative \
+      --force_rerun \
+      --gpu_id 3
+  done
+done
---
+++ scripts/eval_gsm8k/mistral.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# This script runs the experiment
+# test set: mmluaa
+# incontext dataset:
+# model: mistral-7b
+# NOTE: The test size is *not* limited
+
+conda activate inctxt
+
+MODEL_NAME="mistral-7b"
+
+# do not include dailmail, as we have to limit eval_Size
+INCONTEXT_SETS=("gsm8k" "gigaword" "tweetqa")
+
+# For each incontext dataset
+for INCONTEXT_SET in "${INCONTEXT_SETS[@]}"; do
+  echo "Incontext: $INCONTEXT_SET"
+  for i in {0..3..1}; do
+    echo "---Running with $i examples---"
+    python main.py \
+      --eval_data_name gsm8k \
+      --incontext_data_name $INCONTEXT_SET \
+      --num_examples $i \
+      --model_name $MODEL_NAME \
+      --batchsize 1 \
+      --iterative \
+      --force_rerun \
+      --gpu_id 2
+  done
+done
